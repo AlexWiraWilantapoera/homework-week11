de158c14a9ddeade5736196945a6525c56099c98
AlexWiraWilantapoera <alexwira99@gmail.com>
2024-03-22 16:26:55 +0700

    CRUD todo list & unit testing

---
+++ config/config.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+
+const config = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+    port: process.env.PGPORT
+  },
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_TEST,
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+    port: process.env.PGPORT
+  },
+}
+
+module.exports = config
