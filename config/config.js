0f4c8c7fb5c6dad8ea02c1603e6a47536d0ea2b1
AlexWiraWilantapoera <alexwira99@gmail.com>
2024-03-23 09:06:23 +0700

    update

--- config/config.js
+++ config/config.js
@@ -2,24 +2,26 @@ require('dotenv').config()
 
 const config = {
   development: {
-    username: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: "localhost",
-    dialect: "postgres"
+    dialect: "postgres",
+    port: 5432
   },
   test: {
     username: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.DB_TEST,
     host: "localhost",
-    dialect: "postgres"
+    dialect: "postgres",
+    port: 5432
   },
   docker: {
     username: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    host: "localhost",
     dialect: "postgres",
     port: process.env.PGPORT
   },
